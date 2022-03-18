247b7af0fe8d9d3d3d3d26ce86d64a1730669ab1
zcaldwell <mintyst.john@gmail.com>
2022-03-18 15:05:40 -0700

    2nd Test for 2nd Table

--- __tests__/fruit.test.js
+++ __tests__/fruit.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fruit = require('../lib/models/Fruit');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -12,10 +13,17 @@ describe('hand-of-resources routes', () => {
     pool.end();
   });
 
-  it.only('updates a  fruit', async () => {
+  it('updates a  fruit', async () => {
     const expected = { name: 'Peach', isStoneFruit: '1' };
     const res = await request(app).post('/api/v1/fruits').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it.only('get all fruits', async () => {
+    const expected = await Fruit.findAll();
+    const res = await request(app).get('/api/v1/fruits');
+
+    expect(res.body).toEqual(expected);
   });
 });
