e4c184a7b234c43a3f8be14ce2bf6e684c41e6b8
zcaldwell <mintyst.john@gmail.com>
2022-03-20 19:49:18 -0700

    1st test for 5th table

---
+++ __tests__/burgers.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Burger = require('../lib/models/Burger');
+
+describe('hand-of-resources routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should add a new burger', async () => {
+    const expected = {
+      name: 'Mid City Smashburger',
+      rating: 9,
+    };
+    const res = await request(app).post('/api/v1/burgers').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+});
